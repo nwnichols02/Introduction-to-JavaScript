3239792b06e096c49b25cfd2067658c700012071
Nathan Nichols <nwnichols02@gmail.com>
2022-01-11 17:44:12 -0700

    half complete with final task 7

--- index.js
+++ index.js
@@ -262,19 +262,19 @@ Using the grade function below do the following:
 */
 
  function grade(score){
-   if(score >=90 && score <=100){
+   if(score >=90){
      return 'you got an A'; 
-   }else if(score >=80 && score <=89){
+   }else if(score <90 && score >=80){
     return 'you got a B';
-  } else if(score >=70 && score <=79){
+  } else if(score <80 && score >=70){
     return 'you got a C';
-  } else if(score >=60 && score <=69){
+  } else if(score <70 && score >=60){
     return 'you got a D';
   } else if(score <60){
     return 'you got an F';
   }
 }
-
+console.log(grade(70))
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
